db87b1d09438f381754bbe47ce8f2aafa264ff76
Paulo Casaes <pcasaes@gmail.com>
2020-08-27 03:23:57 -0300

    Improve fire sound

--- sounds/BoltFiredSoundHandler.gd
+++ sounds/BoltFiredSoundHandler.gd
@@ -8,9 +8,9 @@ extends Node2D
 const FireSamplePlayer = preload("res://sounds/FireSamplePlayer.tscn")
 
 
-const MAX_SAMPLES = 4
-const MAX_SAMPLES_MINUS_ONE = 3
-const MAX_DISTANCE = 2000
+const MAX_SAMPLES = 8
+const MAX_SAMPLES_MINUS_ONE = 7
+const MAX_DISTANCE = 9000
 
 var cameraStore = CameraStore.store
 var samples = []
@@ -23,16 +23,19 @@ var sinceLastPlayer = 99999
 func _ready():
 	for _i in range(MAX_SAMPLES):
 		var s = FireSamplePlayer.instance()
+		s.max_distance = MAX_DISTANCE
 		prioritySamples.push_back(s)
 		add_child(s)
 		
 		s = FireSamplePlayer.instance()
+		s.max_distance = MAX_DISTANCE
+		s.volume_db = -0.5
 		samples.push_back(s)
 		add_child(s)
 	Server.connect('bolt_fired', self, '_on_fired')
 
 
-func _on_fired(ev, dto):
+func _on_fired(ev, _dto):
 	var priority = ev.get_ownerPlayerId().get_guid() == User.getId()
 	
 	if cameraStore.camera != null and (priority or sinceLastPlayer > 0.1):
@@ -42,17 +45,26 @@ func _on_fired(ev, dto):
 		if priority:
 			var s = prioritySamples[nextPriority]
 			if !s.playing:
-				_play(s, x, y)
+				_play(s, x, y, 0)
 				nextPriority = (nextPriority+1) & MAX_SAMPLES_MINUS_ONE
-		elif abs(x - center.x) < MAX_DISTANCE and abs(y - center.y) < MAX_DISTANCE:
-			var s = samples[next]
-			if !s.playing:
-				_play(s, x, y)
+		elif !samples[next].playing:
+			var disX = pow(x - center.x, 2.0)
+			var disY = pow(y - center.y, 2.0)
+			var dist = sqrt(disY + disX)
+			if dist < MAX_DISTANCE:
+				var s = samples[next]
+				_play(s, x, y, dist)
 				next = (next+1) & MAX_SAMPLES_MINUS_ONE
 	
-func _play(s, x, y):
+func _play(s, x, y, d):
 	s.position.x = x
 	s.position.y = y
+	if d < 2000:
+		s.set_bus("Master")
+	elif d < 4000:
+		s.set_bus("MID")
+	else:
+		s.set_bus("BACK")
 	s.play(0)
 	sinceLastPlayer = 0
 	
