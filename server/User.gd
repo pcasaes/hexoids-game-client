f20d890290eb7fe94e30000ae5985a35c9717bbe
Paulo Casaes <pcasaes@gmail.com>
2022-03-27 22:22:19 -0300

    Fix id comparison

--- server/User.gd
+++ server/User.gd
@@ -18,7 +18,7 @@ func _ready():
 	_config = ConfigFile.new()
 	var err = _config.load(CONFIG_FILE)
 	if err == OK:
-		idBytes = _config.get_value("user", "id-bytes")
+		idBytes = PoolByteArray(_config.get_value("user", "id-bytes"))
 		if idBytes == null:
 			_generate_id()
 		else:
@@ -30,14 +30,15 @@ func _ready():
 		_generate_id()
 
 func _generate_id():
-	idBytes = Uuid.uuidbin()
+	var bytes = Uuid.uuidbin()
+	idBytes = PoolByteArray(bytes)
 	id = Uuid.v4(idBytes)
 	idLongs = Uuid.v4Longs(idBytes)
 	
 	print("id ", id)
 	print("id ", idLongs)
 	
-	_config.set_value("user", "id-bytes", idBytes)
+	_config.set_value("user", "id-bytes", bytes)
 	_config.save(CONFIG_FILE)
 	
 
@@ -55,7 +56,6 @@ func get_username():
 	return username
 
 func is_user_from_guid(guid):
-	return idBytes[0] == guid[0] and idBytes[1] == guid[1] and idBytes[2] == guid[2] and idBytes[3] == guid[3] and idBytes[4] == guid[4] and idBytes[5] == guid[5] and idBytes[6] == guid[6] and idBytes[7] == guid[7] and idBytes[8] == guid[8] and idBytes[9] == guid[9] and idBytes[10] == guid[10] and idBytes[11] == guid[11] and idBytes[12] == guid[12] and idBytes[13] == guid[13] and idBytes[14] == guid[14] and idBytes[15] == guid[15]
-
+	return idBytes == guid
 	
 	
